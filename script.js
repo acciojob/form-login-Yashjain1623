e50d4be1a8b809aca14316d53f7b6dc2e77e87ab
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-08-05 12:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Get the values from the form inputs
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
+
+    // Create the output string with first and last names
+    const output = `${firstName} ${lastName}`;
+
+    // Show an alert with the formatted output
+    alert(output);
 }
